91e6d2217aa5594b4d22bef905704fc34325c91d
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-12-27 22:30:18 +0200

    Добавьте файлы проекта.

---
+++ lab Soroka.D 17/Program.cs
@@ -0,0 +1,13 @@
+﻿using lab;
+
+int[,] nazvanie = new int[6, 6];
+for (int i = 0; i < 6; i++)
+{
+    for (int j = 0; j < 6; j++)
+    {
+        Console.Write($"Введите {i},{j}: ");
+        nazvanie[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+Console.WriteLine(lab.Class1.treper(nazvanie));
---
+++ lab/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace lab
+{
+    public class Class1
+    {
+        public static long treper(int[,] nazvanie)
+        {
+            long reper = 1;
+
+            for (int t = 0; t < 6; t++)
+            {
+                for (int r = 0; r < 6; r++)
+                {
+                    if (t == r)
+                    {
+                        reper *= nazvanie[t, r];
+                    }
+                }
+            }
+            return reper;
+        }
+    }
+}
